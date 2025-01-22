70ed7a61f4729af76cd0bd569a1cce6ec824ae75
muntazir <mntzrmhmd923@gmail.com>
2025-01-22 20:00:50 +0300

    add card and carContainer components

---
+++ src/components/CardContainer.jsx
@@ -0,0 +1,61 @@
+import Card from "./Card";
+
+const data = [
+  {
+    id: 1,
+    name: "Apple_Watch_Ultra_2",
+    price: 500,
+    image: "src/assets/Apple_Watch_Ultra_2-removebg-preview.png",
+    description: "This is a description of the product",
+    company: "apple",
+    category: "Electronics",
+    insert_Date: "2025-01-14",
+    rate: 4.5,
+  },
+  {
+    id: 2,
+    name: "Apple_Watch_Ultra_2",
+    price: 500,
+    image: "src/assets/Xbox_series_x_slide-removebg-preview.png",
+    description: "This is a description of the product",
+    company: "apple",
+    category: "Electronics",
+    insert_Date: "2025-01-21",
+    rate: 4.5,
+  },
+
+  {
+    id: 3,
+    name: "Apple_Watch_Ultra_2",
+    price: 500,
+    image: "src/assets/samsung_gear_camera-removebg-preview.png",
+    description: "This is a description of the product",
+    company: "apple",
+    category: "Electronics",
+    insert_Date: "2025-01-7",
+    rate: 4.5,
+  },
+  {
+    id: 4,
+    name: "Apple_Watch_Ultra_2",
+    price: 500,
+    image: "src/assets/Apple_Watch_Ultra_2-removebg-preview.png",
+    description: "This is a description of the product",
+    company: "apple",
+    category: "Electronics",
+    insert_Date: "2025-01-21",
+    rate: 4.5,
+  },
+];
+
+const CardContainer = () => {
+  return (
+    <div className="flex flex-wrap justify-center space-y-5 space-x-8 min-h-full items-end">
+      {data.map((product, index) => {
+        return <Card data={product} key={index} />;
+      })}
+    </div>
+  );
+};
+
+export default CardContainer;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,15 @@
 import Hero from "../components/Hero"
 import Footer from "../components/Footer"
+import CardContainer from "../components/CardContainer"
 
 function Home(){
     return(
-        <div className="w-full h-full font-body">
+        <div className="w-full h-full font-body flex flex-col space-y-4 bg-body-background">
             <Hero />
+            <h5 className="text-4xl text-center font-bold">
+                Featured Product
+            </h5>
+            <CardContainer />
             <Footer />
         </div>
     )

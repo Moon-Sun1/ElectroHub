1bc07d0809643ec8f3b898c4216ab70ae66d39a5
KarrarAlrsheed <krar.iq2050@gmail.com>
2025-01-25 00:53:19 +0300

    layout fix -[01]

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -17,27 +17,30 @@ export function Dropdown() {
   return (
     <div className="relative inline-block text-left">
       <button
-        onClick={() => setIsOpen(!isOpen)}
-        className="  h-12  inline-flex items-center justify-center w-full px-3 py-1 text-sm font-medium text-gray-700 bg-white  border-r border-gray-300  hover:bg-gray-50 "
-        aria-haspopup="true"
-        aria-expanded={isOpen}
+        onMouseEnter={() => setIsOpen(true)}
+        className="h-12 inline-flex items-center justify-center w-full px-3 py-1 text-sm font-medium text-gray-700 bg-white border-r border-gray-300 hover:bg-gray-50"
+  
       >
         {selected}
         <FaChevronDown
-          className={`ml-2 h-4 w-4 transition-transform duration-200 ${
+          className={`ml-2  w-4 transition-transform duration-200 ${
             isOpen ? "transform rotate-180" : ""
           }`}
         />
       </button>
 
-      {isOpen && ( //show menu if true
-        <div className="absolute right-0 w-56 mt-2 origin-top-right bg-white rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none z-10 transform opacity-100 scale-100 transition-all duration-200">
+      {isOpen && (
+        <div 
+        className="absolute right-0 w-56 mt-2 origin-top-right bg-white rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none z-10">
           <div className="py-1">
             {menuItems.map((item, index) => (
               <Link
-                to={item}
+                to={`/${item}`}
                 key={index}
-                className="block w-full px-4 py-2 text-sm text-gray-700 text-left hover:bg-gray-100 hover:text-gray-900 transition-colors duration-150"
+                className="block w-full px-4 py-2 text-sm text-gray-700 text-left hover:bg-gray-100
+                 hover:text-gray-900 transition-colors duration-150"
+                 onMouseEnter={() => setIsOpen(true)}
+                 onMouseLeave={() => setIsOpen(false)}
                 onClick={() => {
                   setSelected(item);
                   setIsOpen(false);
--- src/components/NavIcons.jsx
+++ src/components/NavIcons.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { FaUser, FaHeart, FaShoppingCart } from "react-icons/fa";
-
+import { Link } from "react-router-dom";
 const NavIcons = () => {
   return (
-    <div className=" hidden md:flex gap-2 sm:gap-3 md:gap-4 bottom-0">
-    <FaUser className="  hover:text-main-green   size-5  md:size-6 lg:size-5 cursor-pointer" />
-    <FaShoppingCart className="  hover:text-main-green  size-5  md:size-6 lg:size-5 cursor-pointer" />
-    <FaHeart className="   hover:text-red-500  size-5  md:size-6 lg:size-5 cursor-pointer" />
+    <div className=" flex md:flex gap-2 sm:gap-3 md:gap-4 bottom-0">
+      <Link to={"/UserInfo"}><FaUser className="  hover:text-main-green   size-5  md:size-6 lg:size-5 cursor-pointer" /></Link>
+      <Link to={"/ShoppingCart"}><FaShoppingCart className="  hover:text-main-green   size-5  md:size-6 lg:size-5 cursor-pointer" /></Link>
+      <Link to={"/Heart"}><FaHeart className="  hover:text-main-green   size-5  md:size-6 lg:size-5 cursor-pointer" /></Link>
+
+
     </div>
   )
 }
